1c5a4bff5ccd00ea5503d9391757317220a23aa9
Miguel Drivet <sphstrife@gmail.com>
2022-09-14 10:49:06 -0500

    Incremento de Codigo
    pswd, guest, rol y footer

--- keywords/keywords_externos.py
+++ keywords/keywords_externos.py
@@ -1,3 +1,4 @@
+from cgi import print_arguments
 import sys, os
 #sys.path.append("E:\\Usuarios\\mdrivets\\Documents\\RobotFramework101")
 sys.path.append(os.path.join(os.getcwd()))
@@ -16,7 +17,6 @@ def abrir_pagina(url):
     pc.abrir_pagina(url)
 
 
-
 @keyword(name="Escribir user ID")
 def escribir_user_id(user_id):
     ac.escribir_usuario(user_id)
@@ -25,3 +25,33 @@ def escribir_user_id(user_id):
 @keyword(name="Click boton login")
 def click_boton_login():
     ac.presionar_boton_login()
+
+
+@keyword(name="Escribir PSWD")
+def escribir_pswd(pswd):
+    ac.escribir_pass(pswd)
+
+
+@keyword(name="Click boton login")
+def click_boton_login():
+    ac.presionar_boton_login()
+
+
+@keyword(name="Click boton guest")
+def click_boton_guest():
+    ac.presionar_boton_guest()
+
+
+@keyword(name="Validar texto de footer")
+def validar_texto_footer(texto_esperado):
+    ac.validar_footer(texto_esperado)
+
+
+@keyword(name="Seleccionar Rol")
+def seleccion_rol(rol):
+    ac.selecciona_rol(rol)
+
+
+@keyword(name="Establecer Rol")
+def establece_rol():
+    ac.presionar_boton_establece_rol()
